4a6335569e3217806d896a2f57f1162a16c8a5b3
Ilya Tkachenko <Ilyatk@users.noreply.github.com>
2012-09-18 09:51:01 +0000

    Add a method to get all announcements

--- src/commands/games.js
+++ src/commands/games.js
@@ -4,3 +4,8 @@ var Games = function() {
 Games.getAnnouncement = function(successCallback, failedCallback) {
     Core.execute('games.getAnnouncement', { version: 2 }, false, successCallback, failedCallback);
 };
+
+// Метод используется для особой внутренней утилиты. В продакшене не использовать.
+Games.getAnnouncementWithUnpublished = function(successCallback, failedCallback) {
+    Core.execute('games.getAnnouncement', { version: 2, isPublished: 0, rnd: Math.random() }, false, successCallback, failedCallback);
+};

da1cc695af9a85a42899688be3a302e1b9c27ac3
Nikolay Bondarenko <misterionkell@gmail.com>
2012-10-11 14:56:38 +0000

    Add the method to get a maintenance data

--- src/commands/games.js
+++ src/commands/games.js
@@ -9,3 +9,9 @@ Games.getAnnouncement = function(successCallback, failedCallback) {
 Games.getAnnouncementWithUnpublished = function(successCallback, failedCallback) {
     Core.execute('games.getAnnouncement', { version: 2, isPublished: 0, rnd: Math.random() }, false, successCallback, failedCallback);
 };
+
+//Метод сложно протестировать на бою, т.к. расписание и вообще его наличие постоянно меняется
+Games.getMaintenance = function(successCallback, failedCallback) {
+    Core.execute('games.getMaintenance', {}, false, successCallback, failedCallback);
+};
+

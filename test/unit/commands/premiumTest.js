73c6a8f65fcea1c430d02208c604453ee2ddd876
Ilya Tkachenko <Ilyatk@users.noreply.github.com>
2013-12-12 10:50:27 +0400

    Add methods to work with premium. Remove non-working tests.

--- test/unit/commands/premiumTest.js
+++ test/unit/commands/premiumTest.js
@@ -1,36 +1,37 @@
-AsyncTestCase("GamesAsyncTest", {
+AsyncTestCase("PremiumAsyncTest", {
     setUp: function () {
-//        Core.setUserId("400001000001634860");
-//        Core.setAppKey("4c2f65777d38eb07d32d111061005dcd5a119150");
+
         Core.setUserId("400001000012483830");
         Core.setAppKey("0f9bdd2498d4eae40d16db171d8c46212636a5a7");
     },
 
-    testGetAnnouncement: function(queue) {
+    testGetStatus: function(queue) {
         var response;
 
         queue.call('Send a request', function(callbacks) {
-            Games.getAnnouncement(callbacks.add(function(body){
+            Premium.getStatus(callbacks.add(function(body){
                 response = body;
             }));
         });
 
         queue.call('Assert the response', function() {
-            assertNotUndefined(response.announcement);
+            assertNotUndefined('Duration', response.duration);
         });
     },
 
-    testGetFacts: function(queue) {
+    testGetGrid: function(queue) {
         var response;
 
         queue.call('Send a request', function(callbacks) {
-            Games.getFacts(callbacks.add(function(body){
+            Premium.getGrid(callbacks.add(function(body){
                 response = body;
             }));
         });
 
         queue.call('Assert the response', function() {
-            assertNotUndefined(response.facts);
+            assertUndefined('Grid', response.error)
+            assertNotUndefined('Grid', response);
+            assertArray('Response should be array', response)
         });
     }
 });

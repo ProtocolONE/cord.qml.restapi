f7d7409480389e44b3344abfdfb7a54f76746740
Ilya Tkachenko <Ilyatk@users.noreply.github.com>
2017-10-03 09:07:04 +0300

    Add method Auth.getCentrifugoToken

--- src/commands/auth.js
+++ src/commands/auth.js
@@ -5,3 +5,6 @@ Auth.getRedirectToken = function(successCallback, failedCallback) {
     Core.execute('auth.getRedirectToken', {}, true, successCallback, failedCallback);
 };
 
+Auth.getCentrifugoToken = function(timestamp, successCallback, failedCallback) {
+    Core.execute('auth.getCentrifugoToken', {timestamp:timestamp}, true, successCallback, failedCallback);
+};
